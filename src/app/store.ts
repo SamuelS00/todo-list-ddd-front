56824cbde61c8c1b1ec8bc1fcd933443076c3546
Samuel <samuelsoutosilva14@gmail.com>
2023-02-13 19:26:49 -0300

    feat: add store - redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,30 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  combineReducers,
+  PreloadedState 
+} from '@reduxjs/toolkit';
+import { apiSlice } from "../features/api/apiSlice";
+import { todosApiSlice } from '../features/todos/todoSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+const rootReducer = combineReducers({
+  [apiSlice.reducerPath]: apiSlice.reducer,
+  [todosApiSlice.reducerPath]: apiSlice.reducer,
 });
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(apiSlice.middleware),
+  });
+};
+
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
